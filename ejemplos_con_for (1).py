396fc1d4abce0fb4f46bcba9869baebe5250e750
valeriao553 <a.23308051281203@cbtis128.edu.mx>
2024-10-24 16:14:30 -0600

    Add files via upload

---
+++ ejemplos_con_for (1).py
@@ -0,0 +1,16 @@
+#Muestra cada fruta de la lista.
+print(" ")
+print("Ordonez Martinez Valeria")
+print(" ")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+#a traves de las letras de la palabra "banana":
+for x in "banana":
+  print(x)
+#salir cuando x es banana
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
