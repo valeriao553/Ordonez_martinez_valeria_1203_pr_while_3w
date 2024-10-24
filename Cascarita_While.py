3882ca5a0c79719f7f014919eada8cac31db0a88
valeriao553 <a.23308051281203@cbtis128.edu.mx>
2024-10-24 16:07:39 -0600

    Add files via upload

---
+++ Cascarita_While.py
@@ -0,0 +1,17 @@
+#While
+print(" ")
+print("Ordonez Martinez Valeria 3W 1203, WHILE")
+print(" ")
+
+print("Dias de la semana:")
+print(" ")
+
+dia=0
+semana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
+while dia < 7:
+    
+    print ("Hoy es " + semana[dia])
+    dia += 1
+   
+    
+    
---
+++ ejemplos_con_for.py
@@ -0,0 +1,14 @@
+#Muestra cada fruta de la lista.
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+#a traves de las letras de la palabra "banana":
+for x in "banana":
+  print(x)
+#salir cuando x es banana
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
