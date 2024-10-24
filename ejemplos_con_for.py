7c4113ca1890f8c793c73ec81ea15575baf37e4e
valeriao553 <a.23308051281203@cbtis128.edu.mx>
2024-10-24 16:15:53 -0600

    Update ejemplos_con_for.py

--- ejemplos_con_for.py
+++ ejemplos_con_for.py
@@ -1,14 +1,1 @@
-#Muestra cada fruta de la lista.
 
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x)
-#a traves de las letras de la palabra "banana":
-for x in "banana":
-  print(x)
-#salir cuando x es banana
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x)
-  if x == "banana":
-    break
